12e7146fa3136c9fe3c9aef402090c3908315df9
Morten Andersson <man@effera.no>
2025-10-03 13:45:12 +0200

    moved gpu out of main

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -1,11 +1,13 @@
 pub mod camera;
 pub mod depth;
+pub mod gpu;
 pub mod objects;
 pub mod primitives;
 pub mod vertex;
 
 pub use camera::CameraUniform;
 pub use depth::Depth;
+pub use gpu::Gpu;
 pub use objects::ObjectData;
 pub use primitives::cube_mesh;
 pub use vertex::Vertex;
