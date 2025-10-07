804f2f73074f3fefec9ff88b6bc37b2782f2b816
Morten Andersson <morten.andersson@gmail.com>
2025-10-07 20:46:22 +0200

    add: blender baked animation scene

--- src/settings.rs
+++ src/settings.rs
@@ -132,8 +132,8 @@ pub struct Resolution {
 impl Default for Resolution {
     fn default() -> Self {
         Self {
-            width: 1280,
-            height: 720,
+            width: 1920,
+            height: 1080,
         }
     }
 }
