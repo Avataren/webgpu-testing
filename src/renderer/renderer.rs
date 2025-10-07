25bc38f53427500b694528bae107cf53063a84a1
Morten Andersson <morten.andersson@gmail.com>
2025-10-07 12:34:18 +0200

    Clean up commented code and remove unused allowances

--- src/renderer/renderer.rs
+++ src/renderer/renderer.rs
@@ -999,27 +999,8 @@ impl LightsBuffer {
         queue.write_buffer(&self.buffer, 0, bytemuck::bytes_of(&data));
         let shadow_data = ShadowsUniform::from_data(lights);
 
-        // DEBUG: Check what we're uploading
-        // if shadow_data.counts[0] > 0 {
-        //     log::info!(
-        //         "Uploading shadow uniform - first dir shadow enabled: {}",
-        //         shadow_data.directionals[0].params[0]
-        //     );
-        //     log::info!(
-        //         "  Matrix first row: {:?}",
-        //         shadow_data.directionals[0].view_proj[0]
-        //     );
-        // }
-
         queue.write_buffer(&self.shadow_buffer, 0, bytemuck::bytes_of(&shadow_data));
     }
-
-    // fn update(&mut self, queue: &wgpu::Queue, lights: &LightsData) {
-    //     let data = LightsUniform::from_data(lights);
-    //     queue.write_buffer(&self.buffer, 0, bytemuck::bytes_of(&data));
-    //     let shadow_data = ShadowsUniform::from_data(lights);
-    //     queue.write_buffer(&self.shadow_buffer, 0, bytemuck::bytes_of(&shadow_data));
-    // }
 }
 
 #[repr(C)]
