8ae546a83aaf0c7d32b4a2a4f60e720ed728a0c5
Morten Andersson <morten.andersson@gmail.com>
2025-10-12 15:33:29 +0200

    wip: web build

--- build_web.sh
+++ build_web.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 RUSTFLAGS="--cfg=web_sys_unstable_apis" \
-    cargo build --target wasm32-unknown-unknown --release --example simple
+    cargo build --target wasm32-unknown-unknown --release --example minimal
 
 wasm-bindgen \
     --target web \
     --out-dir web/pkg \
-    target/wasm32-unknown-unknown/release/examples/simple.wasm
+    target/wasm32-unknown-unknown/release/examples/minimal.wasm
 
 python3 -m http.server --directory web 8080
